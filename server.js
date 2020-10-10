3f1c2d5b48f2b67429b26664dfb52227cde6e11e
priyamishra2903 <priya.1721ec1106@kiet.edu>
2020-10-10 17:00:21 +0530

    update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const {to} = require('await-to-js');
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const {logger} = require("./logging/logger");
+const alert = require('./Sentry/alert');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,7 +20,7 @@ app.use("/orders", require("./routes/order"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (req, res) => {
-   
+    logger.info(`Server started on PORT ${PORT}`);
     console.log(`Server is running on PORT ${PORT}`);
 });
 
